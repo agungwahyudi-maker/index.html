75d3a4806992fcfe8b4f0eb8536c373aee74ae82
agungwahyudi-maker <agungwahyu0122@gmail.com>
2021-10-08 01:27:58 +0700

    baru

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(".project-carousel").owlCarousel({
+  loop: true,
+  autoplay: true,
+  margin: 0,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 2,
+    },
+    1000: {
+      items: 3,
+    },
+  },
+});
